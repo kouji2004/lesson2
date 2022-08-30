8968557a0599eac98bd239f650a49158e007f9cd
kouji2004 <mrtaxi48@gmail.com>
2022-08-30 11:36:39 +0000

    config

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_08_29_140529) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "state"
+    t.text "task"
+    t.date "limit_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
